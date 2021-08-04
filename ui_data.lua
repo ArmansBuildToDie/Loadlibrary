37e5cefadd257359cf696d18b650b0397a5e0baa
ArmansBuildToDie <68616769+ArmansBuildToDie@users.noreply.github.com>
2021-08-04 12:09:44 -0400

    Update ui_data.lua

--- ui_data.lua
+++ ui_data.lua
@@ -30,7 +30,7 @@ local characters = {".", " ", "/", "#", "?"}
 
 for _,get in pairs(characters)do
 	if string.match(title, get) then
-		newTitle = tostring(string.gsub(title, get, "_"))
+		newTitle = tostring(string.gsub(get, get, "_"))
 	end
 end
 
